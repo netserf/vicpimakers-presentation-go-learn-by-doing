b7cfeecf26c4e1c584c79059bbacb3d4fdbc6982
netserf <networkserf@gmail.com>
2022-11-09 16:34:25 -0800

    feat: add example 3 - conditionals

---
+++ examples/03_conditionals.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	num := 5
+	if num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+}
