e9f9cf2a478754fb7fdec3361050813ce52ca684
netserf <networkserf@gmail.com>
2022-11-21 15:47:11 -0800

    feat: add example 12 - pointers

---
+++ examples/12_pointers.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+// Go supports pointers, allowing you to pass references to values and records.
+
+// The &i syntax gives the memory address of i, i.e. a pointer to i.
+
+// Pointers can be printed too.
+
+func zeroValue(integerValue int) {
+	integerValue = 0
+}
+
+func zeroPointer(integerPointer *int) {
+	*integerPointer = 0
+}
+
+func main() {
+	initialInteger := 1
+	fmt.Println("initialInteger:", initialInteger)
+
+	// zeroValue has an int parameter and the arguments will be passed by value.
+	// So zeroValue gets a copy of intialInteger which ensures initialInteger's
+	// original value remains unchanged.
+	zeroValue(initialInteger)
+	fmt.Println("initialInteger after zeroValue():", initialInteger)
+
+	// &initialInteger gives the memory address of the initialInteger.
+	fmt.Println("memory address of the pointer:", &initialInteger)
+
+	// zeroPointer has an *int parameter, meaning that it takes an int pointer.
+	// The *integerPointer code in the function body dereferences the pointer
+	// from its memory address to the current value at that address. So,
+	// assigning a value to the dereferenced pointer changes the value at the
+	// referenced address.
+	zeroPointer(&initialInteger)
+	fmt.Println("initialInteger after zeroPointer():", initialInteger)
+}
