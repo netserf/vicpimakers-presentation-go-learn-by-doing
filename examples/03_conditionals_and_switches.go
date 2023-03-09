881ef2f550609ef6a68470a90638c8ba710f66b3
netserf <networkserf@gmail.com>
2023-03-09 05:47:02 -0800

    feat: example 03 showing fallthrough

--- examples/03_conditionals_and_switches.go
+++ examples/03_conditionals_and_switches.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func main() {
 	num := 5
-
+	// You can use if, else if, else statements to execute different code
+	// blocks based on conditions.
 	fmt.Println("Go supports if, else if, else statements:")
 	if num < 0 {
 		fmt.Println(num, "is negative")
@@ -14,7 +15,8 @@ func main() {
 		fmt.Println(num, "has multiple digits")
 	}
 
-	// also switch / case statements
+	// Go also supports switch / case statements to handle multiple
+	// cases for a single variable.
 	fmt.Println("Go also supports switch / case statements:")
 	i := 2
 	fmt.Print("Write ", i, " as ")
@@ -27,5 +29,23 @@ func main() {
 		fmt.Println("three")
 	default:
 		fmt.Println("unknown")
+	}
+
+	// If you want C-style fall-through behavior, use the fallthrough statement.
+	fmt.Println("Go supports an explicit fallthrough statement:")
+	s := "two"
+	fmt.Println("Show fallthrough for :", s)
+	switch {
+	case s == "one":
+		fmt.Println("1")
+		fallthrough
+	case s == "two":
+		fmt.Println("2")
+		fallthrough
+	case s == "three":
+		fmt.Println("3")
+		fallthrough
+	default:
+		fmt.Println("always shown")
 	}
 }
